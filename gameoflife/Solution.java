02382219460eeb534df5dbf67a4ace0ccab6ec98
ARON-SAMUEL <emmanuel.aron@gmail.com>
2023-04-05 16:54:11 +0200

    Update Solution.java
    05/04/2023 version

--- gameoflife/Solution.java
+++ gameoflife/Solution.java
@@ -2,153 +2,112 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-
 class Solution {
-
-    public static List<Cellule> getNeighbors(int x,int y , Board b){
-        int width = b.width;
-        int height = b.height;
-       
-        Cellule cell = new Cellule(x,y);
-        Cellule east = null;
-        Cellule west = null;
-        Cellule north = null;
-        Cellule south = null;
-        Cellule northWest = null;
-        Cellule northEast = null;
-        Cellule southWest = null;
-        Cellule southEast = null;
-
-        char cellState = cell.getState();  
-        System.err.println("cell state: "+cellState); 
-        List<Cellule> neighbors = new ArrayList<Cellule>();
-
-        if(x+1 <= width)
-            east = new Cellule(x+1,y);
-        if(x-1 >=0)
-            west = new Cellule(x-1,y);
-        if(y-1>=0)
-            north = new Cellule(x,y-1);
-        if(y+1 <= height)
-            south = new Cellule(x,y+1);
-        if(x-1 >=0 && y-1>=0)
-            northWest = new Cellule(x-1,y-1);
-        if(x+1 <= width && y-1>=0)    
-            northEast = new Cellule(x+1,y-1);
-        if(x-1 >=0 && y+1 <= height)
-            southWest = new Cellule(x-1,y+1);
-        if(x+1 <= width && y+1 <= height)
-            southEast = new Cellule(x+1,y+1);     
-        neighbors.add(east);
-        neighbors.add(west);
-        neighbors.add(north);
-        neighbors.add(south);
-        neighbors.add(northWest);
-        neighbors.add(northEast);
-        neighbors.add(southWest);
-        neighbors.add(southEast);
-        
-        return neighbors;
-    }
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int width = in.nextInt();
         int height = in.nextInt();
-        
-        Board b = new Board(width,height,in);
-       // System.err.println(b);
-        
-       // System.err.println(" "+getNeighbors(0, 0, b)) ;
-        Cellule cellule_i_j ;   
+        Cellule[][] cells = new Cellule[200][200];
+        char stateCell;
+        char aliveCell;
+        char deadCell;
+        for (int i = 0; i < height; i++) {
+            String line = in.next();
+            for(int j=0;j< width;j++){
+                stateCell = line.charAt(j);   
+                cells[i][j] = new Cellule(i,j,stateCell);
+               // System.err.print(cells[i][j].getState());
+                int neighborsCell = countAliveNeighborsCell(cells[i][j],width,height);
+                System.err.println("neighborsCell: "+neighborsCell);
+            }
+        }
+
+        System.out.println(cells[0][0]);
+    }
+
+    public static int countAliveNeighborsCell(Cellule c,int width,int height){
+        int cpt = 0;
+        int east;
+        int west;
+        int north;
+        int south;
+        int northEast;
+        int northWest;
+        int southEast;
+        int southWest;
+        if(c.getX()+1 < width ){
+            //EAST
+            east = c.getX()+1;
+            Cellule e = new Cellule(east, c.getY(),c.getState());//c.getState?
+            //TODO get du state east
+            char stateEast;
+            e.setState(stateEast);
+            if(e.getState() == '1')
+                cpt++;
+           // System.err.println("e.getState(): "+e.getState()+" ; X: "+e.getX()+" ; Y: "+e.getY());
+            if(c.getY()-1 > 0){
+                //NORTH-EAST
+                northEast = c.getY()-1;
+                Cellule ne = new Cellule(east, northEast,c.getState());//c.getState?
+                if(ne.getState() == '1')
+                    cpt++;
+               // System.err.println("ne.getState(): "+ne.getState()+" ; X: "+ne.getX()+" ; Y: "+ne.getY());
+            }
+            if(c.getY()+1 > height){
+                //SOUTH-EAST
+                southEast = c.getY()+1;
+                Cellule se = new Cellule(east, southEast,c.getState());//c.getState?
+                if(se.getState() == '1')
+                    cpt++;
+            }
+                
+        }
+        if(c.getX()-1 > 0 ){
+            //WEST
+            west = c.getX()-1;
+            if(c.getY()-1 > 0){
+                //NORTH-WEST
+                northWest = c.getY()-1;
+                Cellule nw = new Cellule(west,northWest,c.getState());//c.getState?
+                if(nw.getState() == '1')
+                    cpt++;
+            }
+            if(c.getY()+1 > height){
+                //SOUTH-WEST
+                southWest = c.getY()+1;
+                Cellule sw = new Cellule(west,southWest,c.getState());//c.getState?
+                if(sw.getState() == '1')
+                    cpt++;
+            }
+                
+        }
+        if(c.getY()-1 > 0){
+            //NORTH
+            north = c.getY()-1;
+            Cellule n = new Cellule(c.getX(),c.getState());//c.getState?
+            if(n.getState() == '1')
+                cpt++;
+        }
+            
+        if(c.getY()+1 > height){
+            //SOUTH
+            south = c.getY()+1;
+            Cellule s = new Cellule(c.getX(),south,c.getState());//c.getState?
+            if(s.getState() == '1')
+                cpt++;
+        }      
+
+        return cpt;
     }
 }
 
-class Board{
-    int height;
-    int width;
-    Cellule[][] cellulesInitiales;
-    Cellule[][] cellulesFinales;
-    String line;
-    char stateCell;
-
-    public void printInitialArrayState(/**Scanner in **/){
-        System.err.println(" * printInitialArrayState * ");
-         //Get (and eventually print) the initial array state.
-        for(int i=0; i<height;i++){
-           // line = in.next();
-            for(int j=0;j<width;j++){
-              //  stateCell = line.charAt(i);
-                //System.err.print(stateCell);
-                //Cell of width i and height j  (creation of the initial board)
-                cellulesInitiales[i][j] = new Cellule(i,j,stateCell);   
-                System.err.print( cellulesInitiales[i][j] );    
-            }
-            System.out.println();
-        }
-    }
-
-    public void initToZeroFinalState(){
-         for(int i=0; i<height;i++){
-            //line = in.next();
-            for(int j=0;j<width;j++){
-                //System.err.print(" i: "+i+" j: "+j);
-                cellulesFinales[i][j] = new Cellule(0,0,'0',0,null);
-            }
-            System.err.println();
-         }
-    }
-
-    public void printFinalArrayState(Scanner in){
-        System.err.println(" * printFinalArrayState * ");
-         //Get (and eventually print) the final array state.
-        for(int i=0; i<height;i++){
-            line = in.next();
-            for(int j=0;j<width;j++){
-                stateCell = line.charAt(i);  
-                cellulesFinales[i][j] = new Cellule(i,j,stateCell);  
-                System.err.print( cellulesFinales[i][j] );
-            }
-            System.out.println();
-        }
-    }
-
-
-    /**
-     * width = x
-     * height = y
-     */
-    public Board(int width , int height, Scanner in){
-        this.height = height;
-        this.width = width;
-        cellulesInitiales = new Cellule[width][height];
-        cellulesFinales = new Cellule[width][height];
-        
-        //Get (and eventually print) the initial array state.
-        printInitialArrayState();
-       
-       // initToZeroFinalState();
-        printFinalArrayState(in);
-       
-    }
-    public String toString(){
-        StringBuilder sb = new StringBuilder(100);
-        for (int i = 0; i < cellulesFinales.length; i++) {
-            for (int j = 0; j < cellulesFinales[i].length; j++) {
-                sb.append(cellulesFinales[i][j] + " ");
-            }
-            sb.append(System.getProperty("line.separator"));
-        }
-        return "\n"+sb.toString();
-    }
-}
-
- class Cellule {
+class Cellule {
         private int x;
         private int y;
         private char state;//alive: state=='1' ; dead: state=='0'
         private int numberOfNeighbors;
-        private int[] neighbors;
+       
     
         public Cellule(int x,int y,char state){
             this.x=x;
@@ -161,13 +120,7 @@ class Board{
             this.state = state;
             this.numberOfNeighbors = numberOfNeighbors;
         }
-        public Cellule(int x,int y,char state, int numberOfNeighbors,int[] neighbors){
-            this.x=x;
-            this.y=y;
-            this.state = state;
-            this.numberOfNeighbors = numberOfNeighbors;
-            this.neighbors = neighbors;
-        }
+        
         public Cellule(int x,int y){
             this.x=x;
             this.y=y;
@@ -237,4 +190,4 @@ class Board{
             return "[x: "+x+"| y: "+y +"| state: "+state+"| numberOfNeighbors: "+numberOfNeighbors+"] ";
            // return ""+state;
         }
-    }
+}
